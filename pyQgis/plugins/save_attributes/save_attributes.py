d3dedecd49a18db3d5767af42e6b2af8702932ff
causmar <causmar97@gmail.com>
2022-11-19 21:51:12 -0500

    Save Attributes plugin completed

--- pyQgis/plugins/save_attributes/save_attributes.py
+++ pyQgis/plugins/save_attributes/save_attributes.py
@@ -4,13 +4,22 @@ import inspect
 from PyQt5.QtWidgets import QAction
 from PyQt5.QtGui import QIcon
 
+from qgis.core import QgsProcessingAlgorithm, QgsApplication
+import processing
+from .save_attributes_provider import SaveAttributesProvider
+
 cmd_folder = os.path.split(inspect.getfile(inspect.currentframe()))[0]
 
 class SaveAttributesPlugin:
     def __init__(self, iface):
         self.iface = iface
+
+    def initProcessing(self):
+        self.provider = SaveAttributesProvider()
+        QgsApplication.processingRegistry().addProvider(self.provider)
     
     def initGui(self):
+        self.initProcessing()
         icon = os.path.join(os.path.join(cmd_folder, 'logo.png'))
         self.action = QAction(QIcon(icon), 'Save Attributes as CSV', self.iface.mainWindow())
         self.action.triggered.connect(self.run)
@@ -18,9 +27,11 @@ class SaveAttributesPlugin:
         self.iface.addToolBarIcon(self.action)
     
     def unload(self):
+        QgsApplication.processingRegistry().removeProvider(self.provider)
         self.iface.removeToolBarIcon(self.action)
         self.iface.removePluginMenu('&Save Attributes', self.action)
         del self.action
     
     def run(self):
-        self.iface.messageBar().pushMessage('Hello from Plugin')
+        processing.execAlgorithmDialog('save_attributes:save_attributes')
+
